abe953ae7e06a166cb3a1804a7cb6c2981b9245a
Alvin Matthew Pratama <alvinmatthew12@gmail.com>
2020-12-25 16:57:56 +0700

    Create sort by action sheet

--- stockdata/Controller/IntradayViewController.swift
+++ stockdata/Controller/IntradayViewController.swift
@@ -26,7 +26,31 @@ class IntradayViewController: UIViewController {
         tableView.register(UINib(nibName: K.Cell.intradayTableViewCell, bundle: nil), forCellReuseIdentifier: K.Cell.intradayTableViewCell)
     }
     
+    @IBAction func sortByPressed(_ sender: UIButton) {
+        showSortByActionSheet()
+    }
+    
+    // MARK:- Sort by
+    
+    func showSortByActionSheet() {
+        let actionSheet = UIAlertController(title: "Sort by", message: "Choose an option", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Open", style: .default, handler: { (UIAlertAction) in  self.sort(by: "open") }))
+        actionSheet.addAction(UIAlertAction(title: "High", style: .default, handler: { (UIAlertAction) in  self.sort(by: "high") }))
+        actionSheet.addAction(UIAlertAction(title: "Low", style: .default, handler: { (UIAlertAction) in  self.sort(by: "low") }))
+        actionSheet.addAction(UIAlertAction(title: "Date & Time", style: .default, handler: { (UIAlertAction) in  self.sort(by: "datetime") }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    func sort(by value: String) {
+        sortButton.setTitle("sort by: \(value)", for: .normal)
+    }
+    
 }
+
+// MARK:- TableView DataSource, Delegage
 
 extension IntradayViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
