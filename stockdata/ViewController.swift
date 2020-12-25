e081688da013310989f8d8d0729f33814e816853
Alvin Matthew Pratama <alvinmatthew12@gmail.com>
2020-12-25 12:26:35 +0700

    Initial Commit

---
+++ stockdata/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stockdata
+//
+//  Created by Alvin Matthew Pratama on 25/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
